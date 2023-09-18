c977e5bed9319d71479d4ebd70320822e83d54b2
nicolasxavierdev <nicolasxavier.dev@gmail.com>
2023-09-18 15:55:29 -0300

    Configura styles global do projeto

---
+++ src/components/Wrapper.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+    background: #c9c9c9;
+`
